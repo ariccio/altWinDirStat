7138a8c74f429b96a8fef98b50258b808df448ba
Alexander Riccio <alexander@riccio.com>
2014-09-27 04:53:48 -0400

    GOT RID OF m_IsReadJobDone!!!!!!!!!!!!!!!!

--- WinDirStat/windirstat/item.cpp
+++ WinDirStat/windirstat/item.cpp
@@ -109,7 +109,7 @@ void readJobNotDoneWork( _In_ CItemBranch* ThisCItem ) {
 
 	if ( dirCount > 0 && fileCount > 1 ) {
 		filesFolder = new CItemBranch { IT_FILESFOLDER, _T( "<Files>" ), 0, zeroInitFILETIME( ), 0, false };
-		filesFolder->SetReadJobDone( false );
+		//filesFolder->SetReadJobDone( false );
 		ThisCItem->AddChild( filesFolder );
 		}
 	else if ( fileCount > 0 ) {
@@ -127,7 +127,7 @@ void readJobNotDoneWork( _In_ CItemBranch* ThisCItem ) {
 		}
 	ThisCItem->UpwardAddSubdirs( dirCount );
 	ThisCItem->UpwardAddReadJobs( -1 );
-	ThisCItem->SetReadJobDone( true );
+	//ThisCItem->SetReadJobDone( true );
 	}
 
 #ifdef _DEBUG
@@ -167,11 +167,11 @@ void DoSomeWork( _In_ CItemBranch* ThisCItem, _In_ _In_range_( 0, UINT64_MAX ) c
 	auto start = GetTickCount64( );
 	auto typeOfThisItem = ThisCItem->m_type;
 	if ( typeOfThisItem == IT_DRIVE || typeOfThisItem == IT_DIRECTORY ) {
-		if ( ThisCItem->IsDone( ) ) {
-			ASSERT( ThisCItem->IsReadJobDone( ) );
-			}
-		if ( !ThisCItem->IsReadJobDone( ) ) {
-			ASSERT( !ThisCItem->IsDone( ) );
+		//if ( ThisCItem->IsDone( ) ) {
+		//	ASSERT( ThisCItem->IsReadJobDone( ) );
+		//	}
+		if ( !ThisCItem->IsDone( ) ) {
+			//ASSERT( !ThisCItem->IsReadJobDone( ) );
 			readJobNotDoneWork( ThisCItem );
 			}
 		if ( GetTickCount64( ) - start > ticks ) {
@@ -179,7 +179,7 @@ void DoSomeWork( _In_ CItemBranch* ThisCItem, _In_ _In_range_( 0, UINT64_MAX ) c
 			}
 		}
 	if ( typeOfThisItem == IT_DRIVE || typeOfThisItem == IT_DIRECTORY || typeOfThisItem == IT_MYCOMPUTER ) {
-		ASSERT( ThisCItem->IsReadJobDone( ) );
+		//ASSERT( ThisCItem->IsReadJobDone( ) );
 		if ( ThisCItem->GetChildrenCount( ) == 0 ) {
 			ASSERT( !ThisCItem->IsDone( ) );
 			ThisCItem->SortAndSetDone( );
@@ -215,11 +215,11 @@ CItemBranch::CItemBranch( ITEMTYPE type, _In_z_ PCTSTR name, std::uint64_t size,
 	auto thisItem_type = m_type;
 	if ( thisItem_type == IT_FILE || dontFollow || thisItem_type == IT_MYCOMPUTER ) {
 		ASSERT( TmiIsLeaf( ) || dontFollow );
-		m_readJobDone = true;
+		//m_readJobDone = true;
 		}
 	else if ( thisItem_type == IT_DIRECTORY ) {
 		UpwardAddReadJobs( 1 );
-		m_readJobDone = false;
+		//m_readJobDone = false;
 		}
 
 	SetAttributes( attr );
