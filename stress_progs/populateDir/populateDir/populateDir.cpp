bde0a16b9fc8cac7d751a1236bbac98349f68620
Alexander Riccio <alexander@riccio.com>
2015-01-24 02:14:23 -0500

    cleanup

--- stress_progs/populateDir/populateDir/populateDir.cpp
+++ stress_progs/populateDir/populateDir/populateDir.cpp
@@ -1,29 +1,19 @@
-//#include <iostream>
 #include <assert.h>
-//#include <fstream>
-//#include <set>
-//#include <sstream>
 #include <vector>
 #include <string>
 #include <Windows.h>
 #include <cstdint>
 //#include <sstream>
 //#include <exception>
-//#include <iostream>
-//#include <ostream>
 #include <string>
 //#include <strsafe.h>
 //#include <memory>
-//#include <ios>
 #include <algorithm>
 #include <functional>
 #include <random>
 #include "Header.h"
 #include <stdio.h>
 #include <utility>
-
-//#define TRACE_OUT(x) std::endl << L"\t\t" << #x << L" = `" << x << L"` "//awesomely useful macro, included now, just in case I need it later.
-//#define TRACE_STR(x) << L" " << #x << L" = `" << x << L"`;"
 
 
 #ifdef NDEBUG
@@ -167,131 +157,7 @@ std::wstring random_string( size_t length, std::function<char( void )> rand_char
 	return str;
 	}
 
-//char randchar( std::uniform_int_distribution<>& dist, std::default_random_engine& rng ) {
-//	return alnumChars[ dist( rng ) ]; 
-//	};
 
-
-//template<typename T>
-//size_t findInRAI( const std::vector<T>& a, const T& b ) {
-//	auto it = std::find( a.begin( ), a.end( ), b );
-//	return it - a.begin( ) ;
-//	}
-
-
-
-//void fixWCout( ) {
-//	if ( !std::wcout.good()) {//Slower than it should be.
-//		auto badBits    = std::wcout.exceptions( );
-//		auto wasBadBit  = badBits & std::ios_base::badbit;
-//		auto wasFailBit = badBits & std::ios_base::failbit;
-//		auto wasEofBit  = badBits & std::ios_base::eofbit;
-//		std::wcout.clear( );
-//		std::wcout << L"wcout was in a bad state!" << std::endl;
-//		std::wcout << TRACE_OUT( wasBadBit );
-//		std::wcout << TRACE_OUT( wasFailBit );
-//		std::wcout << TRACE_OUT( wasEofBit );
-//		std::wcout << std::endl;
-//		}
-//	}
-
-//std::int64_t stdRecurseFind( _In_ std::wstring dir, _In_ bool isLargeFetch, _In_ bool isBasicInfo, _Inout_ std::vector<std::wstring>& files  ) {
-//
-//	std::int64_t num = 0;
-//	dir.reserve( MAX_PATH );
-//	std::wstring normSzDir(dir);
-//	normSzDir.reserve( MAX_PATH );
-//	if ( ( dir.back( ) != L'*' ) && ( dir.at( dir.length( ) - 2 ) != L'\\' ) ) {
-//		dir += L"\\*";
-//		}
-//	else if ( dir.back( ) == L'\\' ) {
-//		dir += L"*";
-//		}
-//
-//	WIN32_FIND_DATA fData;
-//	HANDLE fDataHand = NULL;
-//
-//	if ( isLargeFetch ) {
-//		if ( isBasicInfo ) {
-//			fDataHand = FindFirstFileExW( dir.c_str( ), FindExInfoBasic, &fData, FindExSearchNameMatch, NULL, FIND_FIRST_EX_LARGE_FETCH );
-//			}
-//		else {
-//			fDataHand = FindFirstFileExW( dir.c_str( ), FindExInfoStandard, &fData, FindExSearchNameMatch, NULL, FIND_FIRST_EX_LARGE_FETCH );
-//			}
-//		}
-//	else {
-//		if ( isBasicInfo ) {
-//			fDataHand = FindFirstFileExW( dir.c_str( ), FindExInfoBasic, &fData, FindExSearchNameMatch, NULL, 0 );
-//			}
-//		else {
-//			fDataHand = FindFirstFileExW( dir.c_str( ), FindExInfoStandard, &fData, FindExSearchNameMatch, NULL, 0 );
-//			}
-//		}
-//	if ( fDataHand != INVALID_HANDLE_VALUE ) {
-//		if ( !( wcscmp( fData.cFileName, L".." ) == 0 ) ) {
-//			
-//			}
-//		auto res = FindNextFileW( fDataHand, &fData );
-//		while ( ( fDataHand != INVALID_HANDLE_VALUE ) && ( res != 0 ) ) {
-//
-//			auto scmpVal = wcscmp( fData.cFileName, L".." );
-//			if ( ( !( scmpVal == 0 ) ) && ( !( fData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) ) ) {
-//				files.emplace_back( fData.cFileName );
-//				}
-//			if ( ( fData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ) && ( scmpVal != 0 ) ) {
-//				num += descendDirectory( fData, normSzDir, isLargeFetch, isBasicInfo, files );
-//				}
-//			else if ( ( scmpVal != 0 ) && ( ( ( fData.dwFileAttributes & FILE_ATTRIBUTE_DEVICE ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_INTEGRITY_STREAM ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_NO_SCRUB_DATA ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_OFFLINE ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_SPARSE_FILE ) || ( fData.dwFileAttributes & FILE_ATTRIBUTE_VIRTUAL ) ) ) ) {
-//				
-//				--num;
-//				}
-//			res = FindNextFileW( fDataHand, &fData );
-//			
-//			}
-//		}
-//	
-//	FindClose( fDataHand );
-//	return num;
-//	}
-
-
-//size_t stdWork( _In_ std::wstring arg, _In_ bool isLargeFetch, _In_ bool isBasicInfo, _Inout_ std::vector<std::wstring>& files ) {
-//	fixWCout( );
-//	std::wcout << L"Working on: `" << arg << L"`" << std::endl;
-//	std::int64_t numberFiles = 0;
-//	arg.reserve( MAX_PATH );
-//	if ( arg.length( ) > 3 ) {
-//
-//		auto strCmp = ( arg.compare( 0, 4, arg, 0, 4 ) );
-//		if ( strCmp != 0 ) {
-//			arg = L"\\\\?\\" + arg;
-//			std::wcout << L"prefixed `" << arg << L"`, value now: `" << arg << L"`" << std::endl << std::endl;
-//			numberFiles = stdRecurseFind( arg, isLargeFetch, isBasicInfo, files );
-//			}
-//		else {
-//			numberFiles = stdRecurseFind( arg, isLargeFetch, isBasicInfo, files );
-//			}
-//		}
-//	else {
-//		arg = L"\\\\?\\" + arg;
-//		std::wcout << L"prefixed `" << arg << L"`, value now: `" << arg << L"`" << std::endl << std::endl;
-//		
-//		numberFiles = stdRecurseFind( arg, isLargeFetch, isBasicInfo, files );
-//		}
-//	fixWCout( );
-//	std::wcout << std::endl << L"Number of items: " << files.size( ) << std::endl;
-//	return files.size( );
-//	}
-
-//std::int64_t descendDirectory( _Inout_ WIN32_FIND_DATA& fData, _In_ std::wstring& normSzDir, _In_ bool isLargeFetch, _In_ bool isBasicInfo, _Inout_ std::vector<std::wstring>& files ) {
-//	std::wstring newSzDir = normSzDir;//MUST operate on copy!
-//	newSzDir.reserve( MAX_PATH );
-//	newSzDir += L"\\";
-//	newSzDir += fData.cFileName;
-//	std::int64_t num = 0;
-//	num += stdRecurseFind( newSzDir, isLargeFetch, isBasicInfo, files );
-//	return num;
-//	}
 
 //static_assert( !SUCCEEDED( INVALID_HANDLE_VALUE ), "we got's a problem" );
 
@@ -443,7 +309,6 @@ void do_overlapped_write( _In_ const std::wstring& newStr, _In_ const HANDLE& fi
 	overlapped_io_struct.hEvent = handle_event;
 	overlapped_io_struct.Internal = STATUS_PENDING;
 	static_assert( sizeof( wchar_t ) == sizeof( std::wstring::traits_type::char_type ), "" );
-	const auto data_buffer_size = ( static_cast< DWORD >( newStr.size( ) ) * static_cast< DWORD >( sizeof( decltype( newStr )::traits_type::char_type ) ) );
 	const auto data_buffer_size = ( static_cast< DWORD >( newStr.size( ) ) * static_cast< DWORD >( sizeof( std::wstring::traits_type::char_type ) ) );
 	assert( overlapped_io_struct.Internal == STATUS_PENDING );
 
